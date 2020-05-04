b64735492517a82f36f1ff595c443da4461df9ac
Benjamin Smith <ben.smith.developer@gmail.com>
2020-05-04 15:38:06 -0400

    local changes after templated

--- pkg/cmd/cmd.go
+++ pkg/cmd/cmd.go
@@ -1,8 +1,8 @@
 package cmd
 
 import (
-	"github.com/Benbentwo/go-bin-generic/pkg/cmd/common"
-	"github.com/Benbentwo/go-bin-generic/pkg/version"
+	"github.com/Benbentwo/bens-binary/pkg/cmd/common"
+	"github.com/Benbentwo/bens-binary/pkg/version"
 	"github.com/spf13/viper"
 	"io"
 	"strings"
